d08d9cccd749cc350533ecb1323edc1ed47ef6bf
Sohan Sarurkar <sohansarurkar47@gmail.com>
2023-09-27 12:11:56 +0530

    login routes

--- routes.js
+++ routes.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const authentication = require('./Authentication/authentication');
-const limitter = require('express-rate-limit');
 
 
 // Registration route
-//router.post('/register',registerLimitter, authentication.register);
-
+router.post('/register', authentication.register);
 // Login route
 router.post('/login',authentication.login);
-router.post('/register-dashboard', authentication.register);
-// router.get('/user', authentication.getUserDetails);
-// router.post('/verify', authentication.verifyToken);
-// router.post('/re-verify-mail', authentication.resendToken);
-// router.post('/forgot', authentication.forgotPassword);
-// router.post('/resend-forgot', authentication.resendResetToken);
-// router.post('/reset-password', authentication.resetPassword);
+router.post('/register-dashboard', authentication.register_dashboard);
+router.get('/user', authentication.getUserDetails);
+router.post('/verify', authentication.verifyToken);
+router.post('/re-verify-mail', authentication.resendToken);
+router.post('/forgot', authentication.forgotPassword);
+router.post('/resend-forgot', authentication.resendResetToken);
+router.post('/reset-password', authentication.resetPassword);
+router.put('/setUserOnline/:UserId', authentication.setUserOnline);
+router.put('/setUserOffline/:UserId', authentication.setUserOffline);
+router.put('/users/:UserId/block', authentication.Block);
 
 module.exports = router;
